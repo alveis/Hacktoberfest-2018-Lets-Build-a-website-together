e361a3c6202db3a6b384506f614d52841e1406eb
alveis <michaelngiri@gmail.com>
2018-10-03 09:39:55 +0100

    reduce breakpoint for the header

--- scripts/sticky-header.js
+++ scripts/sticky-header.js
@@ -1,22 +1,6 @@
-
-// // Get the header
-// var header = document.getElementById("nav-links");
-
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
-
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function fixHeader() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
  $(document).ready(function(){
 	   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() - 70;
+	   var navHeight = $( window ).height() - 54;
 			 if ($(window).scrollTop() > navHeight) {
 				 $('#nav-links').addClass('fixed');
 			 }
