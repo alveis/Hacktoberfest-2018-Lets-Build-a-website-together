323bd74f296c1e9095c8f60c4afa783810873f33
alveis <michaelngiri@gmail.com>
2018-10-03 08:37:39 +0100

    fix navbar on top while scrolling

--- scripts/sticky-header.js
+++ scripts/sticky-header.js
@@ -1,16 +1,27 @@
 
-// Get the header
-var header = document.getElementById("header-wrap");
+// // Get the header
+// var header = document.getElementById("nav-links");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function fixHeader() {
-	alert('hi');
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function fixHeader() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+ $(document).ready(function(){
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 70;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('#nav-links').addClass('fixed');
+			 }
+			 else {
+				 $('#nav-links').removeClass('fixed');
+			 }
+		});
+	});
