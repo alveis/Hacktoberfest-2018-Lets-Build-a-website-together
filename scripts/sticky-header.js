81b64854f324cba4fb304c27b3a3eae0d9adc788
alveis <43848687+alveis@users.noreply.github.com>
2018-10-12 06:32:29 +0100

    comment blocks of code

--- scripts/sticky-header.js
+++ scripts/sticky-header.js
@@ -1,5 +1,7 @@
- $(document).ready(function(){
+ // script for making the header fixed at the top
+$(document).ready(function(){
 	   $(window).bind('scroll', function() {
+	//if the page scrolls below a set point, stick the header on the top by adding the classname with default style
 	   var navHeight = $( window ).height() - 54;
 			 if ($(window).scrollTop() > navHeight) {
 				 $('#nav-links').addClass('fixed');
